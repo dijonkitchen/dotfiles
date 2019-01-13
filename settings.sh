1dbfd5e8eb0a81d714211a7d60724b96e41f9982
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2019-01-12 21:14:29 -0800

    fix: add back working directory to tabs
    Also keeps current working directory for new tab creation based
    on existing tab

--- settings.sh
+++ settings.sh
@@ -6,7 +6,7 @@ export GIT_PS1_SHOWUNTRACKEDFILES=1
 export GIT_PS1_SHOWUPSTREAM="auto"
 export GIT_PS1_SHOWCOLORHINTS=1
 
-export PROMPT_COMMAND='__git_ps1 "\w" "\\\$ "'
+export PROMPT_COMMAND=${PROMPT_COMMAND:+$PROMPT_COMMAND ;}'__git_ps1 "\w" "\\\$ "'
 export PS1='\w$(__git_ps1 " (%s)")\$ '
 
 alias cd-='cd -'
