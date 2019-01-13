a95abda0337be1e1563df6d80610fd1c00659099
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2019-01-12 18:28:48 -0800

    build(git): add git prompt options
    To know about:
    staged,
    untracked,
    diff between upstream,
    and color hints in prompt
    
    https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh

--- settings.sh
+++ settings.sh
@@ -1,6 +1,12 @@
 source /usr/local/etc/bash_completion.d/git-completion.bash
 source /usr/local/etc/bash_completion.d/git-prompt.sh
 export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWSTASHSTATE=1
+export GIT_PS1_SHOWUNTRACKEDFILES=1
+export GIT_PS1_SHOWUPSTREAM="auto"
+export GIT_PS1_SHOWCOLORHINTS=1
+
+export PROMPT_COMMAND='__git_ps1 "\w" "\\\$ "'
 export PS1='\w$(__git_ps1 " (%s)")\$ '
 
 alias cd-='cd -'
