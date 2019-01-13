802dcb887d6c921623724fc1ab9a38dc2639fd6a
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2019-01-12 21:32:42 -0800

    feat: bold directory for readability
    comment out unused PS1 prompt

--- settings.sh
+++ settings.sh
@@ -6,8 +6,8 @@ export GIT_PS1_SHOWUNTRACKEDFILES=1
 export GIT_PS1_SHOWUPSTREAM="auto"
 export GIT_PS1_SHOWCOLORHINTS=1
 
-export PROMPT_COMMAND=${PROMPT_COMMAND:+$PROMPT_COMMAND ;}'__git_ps1 "\w" "\\\$ "'
-export PS1='\w$(__git_ps1 " (%s)")\$ '
+export PROMPT_COMMAND=${PROMPT_COMMAND:+$PROMPT_COMMAND ;}'__git_ps1 "\[\e[1m\]\w\[\e[0m\]" "\\\$ "'
+# export PS1='\w$(__git_ps1 " (%s)")\$ '
 
 alias cd-='cd -'
 alias e='emacs'
