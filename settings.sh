701384d8e3b09e9a119fa0c6d8f778f61d7b2b80
JC <11434205+dijonkitchen@users.noreply.github.com>
2020-11-24 07:38:32 -0800

    build(java): remove java settings

--- settings.sh
+++ settings.sh
@@ -3,5 +3,5 @@ source $HOME/dotfiles/git.sh
 source $HOME/dotfiles/alias.sh
 
 export SPACEMACSDIR="~/dotfiles/.spacemacs.d/"
-export JAVA_TOOL_OPTIONS="-Dapple.awt.UIElement=true"
 export PAGER=less
+
