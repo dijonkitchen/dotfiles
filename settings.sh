b2947e80abd4b9c271aaa23fee23c2b71a4ad56e
JC <11434205+dijonkitchen@users.noreply.github.com>
2020-11-25 17:28:24 -0800

    refactor: rename to language name

--- settings.sh
+++ settings.sh
@@ -2,7 +2,7 @@ source $HOME/dotfiles/homebrew.sh
 source $HOME/dotfiles/git.sh
 source $HOME/dotfiles/alias.sh
 
-source $HOME/dotfiles/nodejs.sh
+source $HOME/dotfiles/javascript.sh
 source $HOME/dotfiles/ruby.sh
 
 source $HOME/dotfiles/work.sh
