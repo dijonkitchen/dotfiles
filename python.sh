da56f1aad27862bf4c30f86bad709f3dfb0760c0
Jonathan Chen <11434205+dijonkitchen@users.noreply.github.com>
2021-06-18 14:55:32 -0700

    build: update pyenv setup per instructions

--- python.sh
+++ python.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+
+eval "$(pyenv init -)"
