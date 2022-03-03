995a8da204f28077713637f382221cb5050a7a02
Jonathan Chen <11434205+dijonkitchen@users.noreply.github.com>
2022-03-03 15:11:52 -0800

    build: conditionally use pyenv

--- python.sh
+++ python.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
+if command -v pyenv 1>/dev/null 2>&1; then
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init --path)"
 
-eval "$(pyenv init -)"
+    eval "$(pyenv init -)"
+fi
