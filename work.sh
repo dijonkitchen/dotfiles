38ebc3f2508af6cbcefbb104392569110d88ab5b
Jonathan Chen <11434205+dijonkitchen@users.noreply.github.com>
2022-03-03 10:56:43 -0800

    build: add databases to PATH

--- work.sh
+++ work.sh
@@ -7,6 +7,10 @@ then
   source "$HOME/dotfiles/secrets.sh"
 fi
 
-export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
+# MySQL
+export PATH="/opt/homebrew/opt/mysql@5.7/bin:/usr/local/opt/mysql@5.7/bin:$PATH"
+
+# MongoDB
+export PATH="/opt/homebrew/opt/mongodb-community@4.0/bin:/usr/local/opt/mongodb-community@4.0/bin:$PATH"
 
 [[ -f ~/.robinssh/bin/completion.zsh ]] && . ~/.robinssh/bin/completion.zsh
