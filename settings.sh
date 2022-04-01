e9a253f8bb5ccb04727c6917fadd4aebcbabb6eb
Jonathan Chen <11434205+dijonkitchen@users.noreply.github.com>
2022-04-01 06:48:11 -0700

    fix: escalate secrets to top for environment variables

--- settings.sh
+++ settings.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+if [ -e "$HOME/dotfiles/secrets.sh" ];
+then
+  source "$HOME/dotfiles/secrets.sh"
+fi
+
 source "$HOME/dotfiles/homebrew.sh"
 source "$HOME/dotfiles/git.sh"
 source "$HOME/dotfiles/alias.sh"
 
 source "$HOME/dotfiles/javascript.sh"
 source "$HOME/dotfiles/ruby.sh"
-
-if [ -e "$HOME/dotfiles/secrets.sh" ];
-then
-  source "$HOME/dotfiles/secrets.sh"
-fi
 
 source "$HOME/dotfiles/work.sh"
 
