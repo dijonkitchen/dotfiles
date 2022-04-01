3f4cf96363837642b1c28c9533da7ce8cd09700a
Jonathan Chen <11434205+dijonkitchen@users.noreply.github.com>
2022-04-01 09:20:34 -0700

    refactor: use brew prefix globally

--- homebrew.sh
+++ homebrew.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-# Path for brew
-
-# x86
-test -d /usr/local/bin && export PATH="/usr/local/bin:/usr/local/sbin:~/bin:$PATH"
-
-# ARM
-test -d /opt/homebrew/bin && export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
-
-
 if [ -n "${HOMEBREW_MAIN_USER}" ]; then
     echo "Using 'brew' as HOMEBREW_MAIN_USER: ${HOMEBREW_MAIN_USER}. Enter your password for single Homebrew use."
     alias brew='sudo -Hiu $HOMEBREW_MAIN_USER brew'
 fi
 
+HOMEBREW_PREFIX="$(brew --prefix)"
+
+# Path for brew binaries in multi-user mode
+export PATH="$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin:~/bin:$PATH"
+
+
 if [ -n "${BASH_VERSION}" ]; then
-    # heroku autocomplete setup
+    # Heroku autocomplete setup
     HEROKU_AC_BASH_SETUP_PATH=$HOME/Library/Caches/heroku/autocomplete/bash_setup && test -f $HEROKU_AC_BASH_SETUP_PATH && source $HEROKU_AC_BASH_SETUP_PATH;
 
     # Homebrew bash completion script
     if type brew &>/dev/null; then
-        HOMEBREW_PREFIX="$(brew --prefix)"
         if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
             source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
         else
@@ -30,6 +26,7 @@ if [ -n "${BASH_VERSION}" ]; then
         fi
     fi
 elif [ -n "${ZSH_VERSION}" ]; then
+    # Homebrew zsh completion script
     if type brew &>/dev/null; then
         # must be done before compinit
         FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
--- work.sh
+++ work.sh
@@ -3,9 +3,9 @@
 source "$HOME/dotfiles/python.sh"
 
 # MySQL
-export PATH="/opt/homebrew/opt/mysql@5.7/bin:/usr/local/opt/mysql@5.7/bin:$PATH"
+export PATH="$HOMEBREW_PREFIX/opt/mysql@5.7/bin:$PATH"
 
 # MongoDB
-export PATH="/opt/homebrew/opt/mongodb-community@4.0/bin:/usr/local/opt/mongodb-community@4.0/bin:$PATH"
+export PATH="$HOMEBREW_PREFIX/opt/mongodb-community@4.0/bin:$PATH"
 
 [[ -f ~/.robinssh/bin/completion.zsh ]] && . ~/.robinssh/bin/completion.zsh
