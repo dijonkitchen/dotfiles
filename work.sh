fbbb4abe69544389b92749e4ae60d24cb9f139c1
Jonathan Chen <11434205+dijonkitchen@users.noreply.github.com>
2023-01-23 10:30:31 -0500

    build(work): Android environment

--- work.sh
+++ work.sh
@@ -9,3 +9,8 @@ export PATH="$HOMEBREW_PREFIX/opt/mysql@5.7/bin:$PATH"
 export PATH="$HOMEBREW_PREFIX/opt/mongodb-community@4.0/bin:$PATH"
 
 [[ -f ~/.robinssh/bin/completion.zsh ]] && . ~/.robinssh/bin/completion.zsh
+
+# https://reactnative.dev/docs/environment-setup
+export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_SDK_ROOT/emulator
+export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
