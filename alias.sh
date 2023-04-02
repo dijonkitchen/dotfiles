73af413b2e2f1aeef775301724e954342cf82988
Jonathan Chen <11434205+dijonkitchen@users.noreply.github.com>
2023-04-01 23:26:18 -0400

    refactor: remove brew dependencies and unused completions to simplify
    Allows cancelling brew login for multi-user setup
    and still getting nearly all functionality

--- alias.sh
+++ alias.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
 alias ls='ls -FlaG'
-
-if [ -n "${ZSH_VERSION}" ]; then
-    # For actual `help` pages, similar to bash:
-    # https://zsh.sourceforge.io/Doc/Release/User-Contributions.html#Accessing-On_002dLine-Help
-    unalias run-help 2>/dev/null
-    autoload run-help
-    HELPDIR=$(brew --prefix)/share/zsh/help
-    alias help=run-help
-fi
