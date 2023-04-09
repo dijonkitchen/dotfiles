50a5fac15f37dc42a70778754085305cbb24e413
Jonathan Chen <11434205+dijonkitchen@users.noreply.github.com>
2023-04-09 09:21:19 -0400

    fix: shellcheck errors and update nvm deeper integration

--- javascript.sh
+++ javascript.sh
@@ -54,27 +54,31 @@ if command -v nvm 1>/dev/null 2>&1; then
             fi
         fi
     }
+
+    cdnvm "$PWD" || exit
     alias cd='cdnvm'
-    cd "$PWD" || exit
   elif [ -n "${ZSH_VERSION}" ]; then
     autoload -U add-zsh-hook
+
     load-nvmrc() {
-      local node_version="$(nvm version)"
-      local nvmrc_path="$(nvm_find_nvmrc)"
+      local nvmrc_path
+      nvmrc_path="$(nvm_find_nvmrc)"
 
       if [ -n "$nvmrc_path" ]; then
-        local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+        local nvmrc_node_version
+        nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
 
         if [ "$nvmrc_node_version" = "N/A" ]; then
           nvm install
-        elif [ "$nvmrc_node_version" != "$node_version" ]; then
+        elif [ "$nvmrc_node_version" != "$(nvm version)" ]; then
           nvm use
         fi
-      elif [ "$node_version" != "$(nvm version default)" ]; then
+      elif [ -n "$(PWD=$OLDPWD nvm_find_nvmrc)" ] && [ "$(nvm version)" != "$(nvm version default)" ]; then
         echo "Reverting to nvm default version"
         nvm use default
       fi
     }
+
     add-zsh-hook chpwd load-nvmrc
     load-nvmrc
   fi
