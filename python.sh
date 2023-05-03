d5d307e2d577d45016bec5c6256663d1b7c64463
Jonathan Chen <11434205+dijonkitchen@users.noreply.github.com>
2023-05-03 11:51:35 -0400

    build(python): add direnv support

--- python.sh
+++ python.sh
@@ -6,6 +6,10 @@ if command -v pyenv 1>/dev/null 2>&1; then
     eval "$(pyenv init -)"
 fi
 
+if command -v direnv 1>/dev/null 2>&1; then
+    eval "$(direnv hook bash)"
+fi
+
 # Poetry PATH
 if [ -d "$HOME/.local/bin/" ]; then
     export PATH="$HOME/.local/bin:$PATH"
