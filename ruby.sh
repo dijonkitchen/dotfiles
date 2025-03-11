cae81fa64e859eab5756430faa1c8074e74caed7
JC (Jonathan Chen) <11434205+dijonkitchen@users.noreply.github.com>
2025-03-10 20:47:09 -0400

    build: no need for manual YJIT, automatic on Ruby 3.3+, deprecate rbenv for mise

--- ruby.sh
+++ ruby.sh
@@ -4,8 +4,3 @@ alias be="bundle exec"
 
 # Rust initialization for Ruby YJIT
 . "$HOME/.cargo/env"
-export RUBY_YJIT_ENABLE=1
-
-#if command -v rbenv 1>/dev/null 2>&1; then
-#  eval "$(rbenv init -)"
-#fi
