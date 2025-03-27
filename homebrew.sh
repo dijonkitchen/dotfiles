356dfe4ab5ea8b30b5863077cbe8d94b92f56f37
JC (Jonathan Chen) <jonathan.chen@springhealth.com>
2025-03-27 10:37:03 -0400

    fix: load completions and not see error

--- homebrew.sh
+++ homebrew.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+if [ "$(basename "$SHELL")" = "zsh" ]; then
+  # For shell completions
+  autoload -Uz compinit
+  compinit
+fi
+
